b94b6b59abc02b21942d343badc10544e725eca7
DanielBR0612 <danielfranciscobragadearaujo@gmail.com>
2025-10-16 10:52:43 -0300

    tailwind

--- hello/src/app/app.ts
+++ hello/src/app/app.ts
@@ -1,20 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { PanelModule } from 'primeng/panel';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  imports: [RouterOutlet,ButtonModule,PanelModule],
+  template: `<main>
+    <p-panel header = "ola, {{ title() }}">
+      <p class="font-bold text-6xl">Valor do contado: {{ qtd() }}</p>
+      <p-button label="Incrementar 1" (onClick)="incrementar()"></p-button>
+    </p-panel>
+  </main>`,
+    
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('hello');
+  protected readonly title = signal('Daniel');
+
+  protected qtd = signal(0);
+
+  incrementar() {
+    this.qtd.set(this.qtd() + 1);
+  }
 }
 
-// @Component({
-//     selector: 'button-demo',
-//     templateUrl: './button-demo.html',
-//     imports: [ButtonModule]
-// })
-// export class ButtonDemo {}
